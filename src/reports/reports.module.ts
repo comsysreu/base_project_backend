e66dd72fdf9b8bb800c0700d8dcb4915403b66f6
Carlos Gómez <carlos.gomez@blautech.us>
2019-08-21 11:12:52 -0600

    REMOVE DATABASEMODULE IN REPORT MODULE

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
-import { DatabaseModule } from '../common/database/database.module';
+// import { DatabaseModule } from '../common/database/database.module';
 import {  reportsProviders} from './reports.provider';
 
 @Module({
-  imports: [DatabaseModule],
+  // imports: [DatabaseModule],
   controllers: [ReportsController],
   // providers: [ReportsService ,... reportsProviders],
 })
